f09080c650df029390556dadf7697c76db642624
dikshantdarvis <dikshant@darvisstudio.com>
2023-06-13 12:53:53 +0545

    Bare Minimum Docker serup

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Use the official Node.js v14.x image as the base image
+FROM node:14
+
+# Set the working directory inside the container
+WORKDIR /app
+
+# Copy package.json and package-lock.json to the working directory
+COPY package*.json ./
+
+# Install dependencies
+RUN npm install
+
+# Copy the server.js file from the root of the project to the working directory
+COPY server.js .
+
+# Expose port 3000
+EXPOSE 3000
+
+# Start the web service
+CMD ["node", "server.js"]
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// Connect to MongoDB
+mongoose.connect('mongodb://mongo:27017/mydatabase', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('Failed to connect to MongoDB', err));
+
+// Define routes
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
